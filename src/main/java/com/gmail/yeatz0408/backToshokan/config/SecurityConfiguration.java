284b78c8f4381c3ebc1b82453ec6767355c6aa40
unknown <yeatz0408@gmail.com>
2023-05-21 16:07:40 +0900

    configure security again

--- src/main/java/com/gmail/yeatz0408/backToshokan/config/SecurityConfiguration.java
+++ src/main/java/com/gmail/yeatz0408/backToshokan/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.gmail.yeatz0408.backToshokan.config;
 
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
